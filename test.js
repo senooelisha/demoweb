d60c0c75e1c20fefbe229caea71abbcaa6f00ec8
SENOO Elisha  Elikem Kofi <senooelisha.k@actpro.co.jp>
2023-04-14 11:56:09 +0900

    styles updated

--- test.js
+++ test.js
@@ -1,5 +1,35 @@
-alert(
-    "WOOOOW"
-)
+// Set the App ID and API Token for your Kintone application
+const appId = 176;
+const apiToken = 'hBTd9nwPPPiiehx3CnPtUuG4byUJvTUmy8j2ZIAZ';
+const domain = 'smartexchange.cybozu.com'
 
-console.log('The JavaScript file has been loaded and executed successfully!');
+// Define the endpoint for the Kintone REST API
+const apiEndpoint = `https://${domain}/k/v1/records.json`;
+
+// Set the parameters for the API request
+const params = {
+  app: appId,
+  query: '',
+  totalCount: true
+};
+
+// Define the headers for the API request
+const headers = {
+  'X-Cybozu-API-Token': apiToken
+};
+
+// Send the API request using jQuery's AJAX function
+$.ajax({
+  url: apiEndpoint,
+  type: 'GET',
+  data: params,
+  headers: headers
+}).done(function(response) {
+  // Handle the response from the Kintone API
+  console.log("Success")
+  console.log(response);
+}).fail(function(jqXHR, textStatus, errorThrown) {
+  // Handle any errors that occur during the API request
+  console.log("Failed")
+  console.error(jqXHR, textStatus, errorThrown);
+});
