aa8ea9948bead18346b911d362b7a521bb9861ad
SENOO Elisha  Elikem Kofi <senooelisha.k@actpro.co.jp>
2023-04-24 11:55:23 +0900

    WIP

--- jquery-fetch.js
+++ jquery-fetch.js
@@ -1,19 +1,46 @@
+const DOMAIN = 'smartexchange';
+const APP_ID = 176; // Replace with your app ID
+const API_TOKEN = 'o6FeN4CrqHvInaxruscPfU7TCuexjA1BohOqtqpZ';
+const QUERY = ''; // Replace with your query
+const MAX_DATA_SIZE = 300
+
 $(document).ready(function() {
-    var appId = 176; // replace with your app ID
-    var token = "o6FeN4CrqHvInaxruscPfU7TCuexjA1BohOqtqpZ"; // replace with your API token
-    var query = "";
-  
+  let allRecords = [];
+
+  function getRecords(offset) {
+    console.log("records");
+
+    const limit = 100; // Set the limit to 100 records per request
+    const url = `https://${DOMAIN}.cybozu.com/k/v1/records.json?app=${APP_ID}&query=${QUERY}&offset=${offset}&limit=${limit}`;
     $.ajax({
-      url: "https://smartexchange.cybozu.com/k/v1/records.json?app="+appId+"&query="+query+"&totalCount=true",
-      type: "GET",
-      dataType: "json",
-      headers: {"X-Cybozu-API-Token": token},
+      url: url,
+      method: 'GET',
+      data: {
+        url: url,
+        headers: {
+          'X-Cybozu-API-Token': API_TOKEN
+        }
+      },
       success: function(response) {
-        console.log(response);
+        // const records = response.records;
+        allRecords = allRecords.concat(response.records);
+
+        if (MAX_DATA_SIZE > allRecords.length) {
+          // If there are more records, make another request with a new offset
+          getRecords(offset + limit);
+        } else {
+          // All records have been fetched, do something with the data
+          console.log(allRecords);
+        }
       },
       error: function(xhr, status, error) {
-        console.log("Error: " + error);
+        console.log('Error:', error);
       }
     });
-  });
+  }
 
+  // Call the function to fetch the first batch of records
+  getRecords(0);
+  getRecords(100);
+  getRecords(200);
+});
