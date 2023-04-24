9e11a146841a82a3b09cdf44456e863f64afb426
SENOO Elisha  Elikem Kofi <senooelisha.k@actpro.co.jp>
2023-04-24 19:24:37 +0900

    WIP

--- jquery-fetch.js
+++ jquery-fetch.js
@@ -2,45 +2,70 @@ const DOMAIN = 'smartexchange';
 const APP_ID = 176; // Replace with your app ID
 const API_TOKEN = 'o6FeN4CrqHvInaxruscPfU7TCuexjA1BohOqtqpZ';
 const QUERY = ''; // Replace with your query
-const MAX_DATA_SIZE = 300
 
 $(document).ready(function() {
   let allRecords = [];
 
   function getRecords(offset) {
-    console.log("records");
-
     const limit = 100; // Set the limit to 100 records per request
-    const url = `https://${DOMAIN}.cybozu.com/k/v1/records.json?app=${APP_ID}&query=${QUERY}&offset=${offset}&limit=${limit}`;
+    const url = `https://${DOMAIN}.cybozu.com/k/v1/records.json?_=${Math.random()}`;
     $.ajax({
       url: url,
       method: 'GET',
       data: {
-        url: url,
+        // url: url,
+        app: APP_ID,
+        query: QUERY,
+        limit: limit,
+        offset: offset,
+        totalCount: true,
         headers: {
           'X-Cybozu-API-Token': API_TOKEN
         }
       },
       success: function(response) {
-        // const records = response.records;
-        allRecords = allRecords.concat(response.records);
-
-        if (MAX_DATA_SIZE > allRecords.length) {
+        const records = response.records;
+        allRecords = allRecords.concat(records);
+        if (response.totalCount > allRecords.length) {
           // If there are more records, make another request with a new offset
           getRecords(offset + limit);
         } else {
           // All records have been fetched, do something with the data
-          console.log(allRecords);
+          console.log(allRecords.map(item => item.countryName.value));
+          return allRecords.map(item => item.countryName.value);
         }
       },
       error: function(xhr, status, error) {
+        return null;
         console.log('Error:', error);
       }
     });
   }
 
-  // Call the function to fetch the first batch of records
-  getRecords(0);
-  getRecords(100);
-  getRecords(200);
+
+  (function () {
+    "use strict";
+    var countriesDropdown = "countriesDropdown";
+    var events = [
+      "app.record.detail.show",
+      "app.record.edit.show",
+      "app.record.create.show",
+    ];
+
+    kintone.events.on(events, function () {
+        var countriesSel = document.createElement('select');
+
+        countriesList = getRecords(0);
+        countriesList.forEach(element => {
+            var countriesOp = document.createElement('option');
+            countriesOp.value = element;
+            countriesOp.innerHTML = element;
+
+            countriesSel.appendChild(countriesOp);
+        });
+        
+        kintone.app.record.getSpaceElement(countriesDropdown).appendChild(countriesSel);
+    });
+  })();
+  
 });
